0ff7ce225f83ab701d53318ed43f71d8daf76b86
DhirenShivdasani <51030977+DhirenShivdasani@users.noreply.github.com>
2024-01-30 13:48:37 -0500

    Automated data update

--- scrape_odds.py
+++ scrape_odds.py
@@ -31,7 +31,7 @@ if response.status_code == 200:
     master_df['PlayerName'] = master_df['firstName'] + ' ' + master_df['lastName']
     master_df.drop(['firstName', 'lastName'], axis =1, inplace = True)
 
-    sportsbooks = ['draftkings', 'fanduel', 'mgm', 'pointsbet']
+sportsbooks = ['draftkings', 'fanduel', 'mgm', 'pointsbet']
 props = ['pts', 'reb', 'ast', 'ptsrebast', 'ptsreb', 'ptsast', 'rebast']
 
 # Flatten each prop for each sportsbook into separate DataFrames
@@ -54,32 +54,33 @@ for prop in props:
         temp_df['Over_Under'] = temp_df['Over_Under'].apply(lambda x: 'Over' if 'Over' in x else 'Under')
 
         # Combine odds and value into a single column
-        temp_df['Odds_Value'] = temp_df.apply(lambda row: f"({row[f'{sportsbook}_{prop}']}) {row['Odds']}", axis=1)
-
+        temp_df['Odds_Value'] = temp_df.apply(
+                    lambda row: f"({row[f'{sportsbook}_{prop}']}) {row['Odds']}" if pd.notnull(row[f'{sportsbook}_{prop}']) else None, 
+                    axis=1
+                )
         flattened_dfs.append(temp_df)
 
-    # Step 2: Create a Unified Prop Column
-    consolidated_df = pd.concat(flattened_dfs)
+# Step 2: Create a Unified Prop Column
+consolidated_df = pd.concat(flattened_dfs)
 
-    # Step 3: Consolidate Sportsbook Odds and Values
-    pivot_df = consolidated_df.pivot_table(index=['PlayerName', 'team', 'opp', 'Prop', 'Over_Under'], 
-                                        columns='Sportsbook', 
-                                        values='Odds_Value', 
-                                        aggfunc='first').reset_index()
-    prop_mapping = {
-        'pts': 'Points',
-        'reb': 'Rebounds',
-        'ast': 'Assists',
-        'ptsast': 'Pts+Asts',
-        'ptsreb': 'Pts+Rebs',
-        'ptsrebast': 'Pts+Rebs+Asts',
-        'rebast': 'Rebs+Asts'
-    }
+# Step 3: Consolidate Sportsbook Odds and Values
+pivot_df = consolidated_df.pivot_table(index=['PlayerName', 'team', 'opp', 'Prop', 'Over_Under'], 
+                                    columns='Sportsbook', 
+                                    values='Odds_Value', 
+                                    aggfunc='first').reset_index()
+prop_mapping = {
+    'pts': 'Points',
+    'reb': 'Rebounds',
+    'ast': 'Assists',
+    'ptsast': 'Pts+Asts',
+    'ptsreb': 'Pts+Rebs',
+    'ptsrebast': 'Pts+Rebs+Asts',
+    'rebast': 'Rebs+Asts'
+}
 
-    # Step 2: Apply the Mapping
-    pivot_df['Prop'] = pivot_df['Prop'].replace(prop_mapping)
-    pivot_df.replace('(nan) nan', None, inplace=True)
+# Step 2: Apply the Mapping
+pivot_df['Prop'] = pivot_df['Prop'].replace(prop_mapping)
+pivot_df.replace('() nan', None, inplace=True)
 
-    pivot_df.drop([ 'team'], axis = 1, inplace = True)
-    pivot_df.reset_index(drop=True, inplace=True)
-    pivot_df.to_csv('over_under_odds.csv', index = False)
+pivot_df.drop([ 'team'], axis = 1, inplace = True)
+pivot_df.to_csv('over_under_odds.csv', index = False)
